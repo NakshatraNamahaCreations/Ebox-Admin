4fb0687a1e70aab44025469c44bda63694eb2e1f
Nakshatra Namaha Creations <119853069+NakshatraNamahaCreations@users.noreply.github.com>
2024-10-18 14:41:29 +0530

    redeploy
    redeploy

--- src/api-services/apiContents.js
+++ src/api-services/apiContents.js
@@ -1,6 +1,6 @@
 const apiUrl = {
   // BASEURL: "http://192.168.1.103:9000/api",
-  IMAGEURL: "http://192.168.1.103:9000/",
+  IMAGEURL: "http://54.153.132.86:9000/",
   BASEURL: "http://54.153.132.86:9000/",
   // IMAGEURL: "http://localhost:9000/",
   // LOCALURL: "https://eventbox.nakshatranamahacreations.in/api",
